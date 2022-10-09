a678fd0c258a045c15dd160d3d3b9dc534f2d4a1
Sean Rafferty <seanr@randox.com>
2022-10-09 20:32:39 +0100

    simple tweaks for routing

--- src/bibs-portal/src/app/app-routing.module.ts
+++ src/bibs-portal/src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  {
+    path: 'generators',
+    loadChildren: () =>
+        import('./generators/generators.module').then(
+            (m) => m.GeneratorsModule
+        )
+  },
 ];
 
 @NgModule({
--- src/bibs-portal/src/app/generators/generators-routing.module.ts
+++ src/bibs-portal/src/app/generators/generators-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneratorBasicComponent } from './generator-basic/generator-basic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'basic', pathMatch: 'full' },
+  { path: 'basic', component: GeneratorBasicComponent },
+];
 @NgModule({
   declarations: [],
   imports: [CommonModule, RouterModule.forChild(routes)],
