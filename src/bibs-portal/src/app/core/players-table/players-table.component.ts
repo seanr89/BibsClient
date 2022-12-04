d648ba5c2ecc6d2c0ec21f718cbd27f1967d0197
Sean Rafferty <seanr@randox.com>
2022-12-04 21:33:57 +0000

    Tables work

--- src/bibs-portal/src/app/core/players-table/players-table.component.ts
+++ src/bibs-portal/src/app/core/players-table/players-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 import { IPlayer } from 'src/app/models/iplayer';
 
 @Component({
@@ -7,10 +8,11 @@ import { IPlayer } from 'src/app/models/iplayer';
   styleUrls: ['./players-table.component.scss']
 })
 export class PlayersTableComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'email', 'rating'];
+  displayedColumns: string[] = ['name', 'email', 'rating', 'actions'];
   dataSource: IPlayer[] = [];
   @Input('childToMaster') inputPlayer: IPlayer | undefined;
   initialized=false;
+  @ViewChild(MatTable) table: MatTable<IPlayer> | undefined;
 
   constructor() { }
 
@@ -22,6 +24,15 @@ export class PlayersTableComponent implements OnInit {
     if(this.initialized == false)
       return;
     this.dataSource.push(changes['inputPlayer'].currentValue);
+    this.table!.renderRows();
+  }
+
+  onEdit(element: IPlayer): void{
+
+  }
+
+  onDelete(element: IPlayer): void {
+
   }
 
 }
